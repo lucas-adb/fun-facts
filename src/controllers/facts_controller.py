f96a19e2a9c08f7e32960717e778e204e72f1f72
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-02-02 09:04:37 -0300

    feat: get a fact by its id

--- src/controllers/facts_controller.py
+++ src/controllers/facts_controller.py
@@ -1,4 +1,4 @@
-# from bson import ObjectId
+from bson import ObjectId
 from flask import Blueprint, jsonify
 # from flask import Blueprint, jsonify, request
 from models.facts_model import FactsModel
@@ -8,6 +8,7 @@ from utils.http_status import HttpStatus
 facts_controller = Blueprint("facts", __name__)
 
 
+# Gets all facts
 def _get_all_facts():
     facts = FactsModel.find()
     return [fact.to_dict() for fact in facts]
@@ -19,9 +20,20 @@ def facts_index():
     return jsonify(facts_list), HttpStatus.OK.value
 
 
-# def _get_fact(id: str):
-#     return FactsModel.find_one({"_id": ObjectId(id)})
+# Get a fact by its id
+def _get_fact(id: str):
+    return FactsModel.find_one({"_id": ObjectId(id)})
 
+
+@facts_controller.route("/<id>", methods=["GET"])
+def fact_show(id: str):
+    fact = _get_fact(id)
+    if fact is None:
+        return jsonify({"error": "Fact not found"}), HttpStatus.NOT_FOUND
+    return jsonify(fact.to_dict()), HttpStatus.OK.value
+
+
+# Get a random fact
 # @facts_controller.route("/random", methods=["GET"])
 # def fact_random():
 #     fact = FactsModel.get_random()
@@ -36,11 +48,3 @@ def facts_index():
 #     new_music = MusicModel(request.json)
 #     new_music.save()
 #     return jsonify(new_music.to_dict()), HttpStatus.CREATED
-
-
-# @music_controller.route("/<id>", methods=["GET"])
-# def music_show(id: str):
-#     music = _get_music(id)
-#     if music is None:
-#         return jsonify({"error": "Music not found"}), HttpStatus.NOT_FOUND
-#     return jsonify(music.to_dict()), HttpStatus.OK
